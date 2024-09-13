ff8000bae2f19b990b08a52cdaa4076c7bef8516
ChrisW19 <chris.wiseman74@gmail.com>
2024-09-13 15:45:06 -0700

    Add Album and implemented ReduxToolkit

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -36,7 +36,7 @@ function UsersList() {
 
     return <div>
                 <div className="flex flex-row justify-between items-center m-3">
-                    <h1 className="m-2 text-xl">Users</h1>
+                    <h1 className="m-2 text-xl font-bold">Users</h1>
                     <Button loading={isCreatingUser} onClick={handleUserAdd}>
                         + Add User
                     </Button>
